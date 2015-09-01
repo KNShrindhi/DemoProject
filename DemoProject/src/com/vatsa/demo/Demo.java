ef4c3096ca9f0e021252915b6948349007dc83ac
KN Shrinidhi <karanam.shrinidhi@gmail.com>
2015-09-01 18:27:46 +0530

    d

--- DemoProject/src/com/vatsa/demo/Demo.java
+++ DemoProject/src/com/vatsa/demo/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HI Srivatsa");  
-		System.out.println("Hope it works fine  ");
+		System.out.println("Hope it works fine    ");
 	}
 
 }
