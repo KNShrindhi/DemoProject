93c3b0cc172031439f1177b2ee5be7b86a784e2b
richawep <30290978+richawep@users.noreply.github.com>
2018-02-21 16:12:53 +0530

    Update Demo.java
    demo

--- DemoProject/src/com/vatsa/demo/Demo.java
+++ DemoProject/src/com/vatsa/demo/Demo.java
@@ -6,6 +6,7 @@ public class Demo {
 		// TODO Auto-generated method stub
 		//System.out.println("HI Srivatsa");  
 		System.out.println("Hope it works fine");
+		System.out.println("Demo Hi);
 	}
 
 }
